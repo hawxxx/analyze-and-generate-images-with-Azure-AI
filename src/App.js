645735d76317821300b3ba07d09d4ccdf3c3050e
hawxxx <cristhianhack24@hotmail.com>
2023-11-26 21:50:36 -0700

    Updated App.js and added styling in App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Analyzer and Generator</h1>
+        <input type="text" placeholder="Enter image URL or prompt" className="input-field" />
+        <div className="button-group">
+          <button className="button">Analyze Image</button>
+          <button className="button">Generate Image</button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
