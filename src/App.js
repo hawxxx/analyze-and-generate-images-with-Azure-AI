cb7372fb14660c957ef086d8a47f6efbefe2e8bd
hawxxx <cristhianhack24@hotmail.com>
2023-11-26 23:17:12 -0700

    Updated app.js and console messages on Image Analysis AI handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import analyzeImage from './azure-image-analysis';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [analysisResult, setAnalysisResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleAnalyzeClick = async () => {
+    setLoading(true);
+    setAnalysisResult(null);
+    try {
+      const result = await analyzeImage(imageUrl);
+      setAnalysisResult(result);
+    } catch (error) {
+      console.error('Error analyzing image:', error);
+    }
+    setLoading(false);
+  };
+
+  const DisplayResults = () => {
+    if (!analysisResult) return null;
+    return (
+      <div>
+        <h3>Analysis Results:</h3>
+        <pre>{JSON.stringify(analysisResult, null, 2)}</pre>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Image Analyzer and Generator</h1>
-        <input type="text" placeholder="Enter image URL or prompt" className="input-field" />
+        <h1>Image Analyzer and Generator using Azure AI</h1>
+        <input type="text" placeholder="Enter image URL or prompt" className="input-field" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
         <div className="button-group">
-          <button className="button">Analyze Image</button>
+          <button className="button" onClick={handleAnalyzeClick} disabled={loading}>Analyze Image</button>
           <button className="button">Generate Image</button>
         </div>
+        {loading && <p>Loading...</p>}
+        <DisplayResults />
       </header>
     </div>
   );
--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -1,9 +1,10 @@
-// Azure Image Analysis
+// azure-image-analysis.js
 const analyzeImage = async (imageUrl) => {
-    const subscriptionKey = 'process.env.REACT_APP_AZURE_SUBSCRIPTION_KEY';
-    const endpoint = 'process.env.REACT_APP_AZURE_ENDPOINT';
-  
-    const response = await fetch(`${endpoint}vision/v4.0/analyze?visualFeatures=Categories,Description,Color`, {
+    const subscriptionKey = process.env.REACT_APP_AZURE_VISION_KEY;
+    const endpoint = process.env.REACT_APP_AZURE_VISION_ENDPOINT;
+    console.log('Endpoint:', endpoint);
+    console.log('Subscription Key:', subscriptionKey);
+    const response = await fetch(`${endpoint}vision/v4.0/analyze?visualFeatures=Categories,Description,Color,Caption`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
